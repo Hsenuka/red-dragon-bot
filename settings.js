5dbda6f71cc2821b2ae644bf3a9408ea054c9272
Hsenuka <118550195+Hsenuka@users.noreply.github.com>
2023-05-06 20:20:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
 global.vcardowner = ['+94764125996'] // owner number
-global.ownername = "Hirun Senuka" // owner name
+global.ownername = "Hiruna S" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
